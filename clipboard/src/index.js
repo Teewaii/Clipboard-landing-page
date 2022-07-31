f810165f124784b057f875c9be1afe916499cbd8
Taiye Ajimati <taiyeajimati@gmail.com>
2022-07-31 13:23:32 +0400

    s

--- clipboard/src/index.js
+++ clipboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hero from './components/Hero/Hero';
 import Track from './components/Track/Track';
