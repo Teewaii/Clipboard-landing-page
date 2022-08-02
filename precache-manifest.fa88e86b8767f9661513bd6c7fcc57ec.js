d002c1bd55b5d67a0e312e481d5a031b38a81b78
Taiye Ajimati <taiyeajimati@gmail.com>
2022-08-02 13:13:07 +0400

    Updates

---
+++ precache-manifest.fa88e86b8767f9661513bd6c7fcc57ec.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "95396a1dfc723d02f765",
+    "url": "/clipboard-landing-page/static/css/main.a732d6ef.chunk.css"
+  },
+  {
+    "revision": "95396a1dfc723d02f765",
+    "url": "/clipboard-landing-page/static/js/main.7e932193.chunk.js"
+  },
+  {
+    "revision": "f469a86146f1c053f8c6",
+    "url": "/clipboard-landing-page/static/js/runtime~main.c78a0ce5.js"
+  },
+  {
+    "revision": "d2a928265acd936b0b9e",
+    "url": "/clipboard-landing-page/static/js/2.02e209c3.chunk.js"
+  },
+  {
+    "revision": "f019a8d8b6b121e06049",
+    "url": "/clipboard-landing-page/static/js/3.ea86ce1b.chunk.js"
+  },
+  {
+    "revision": "28caa0248db688b9dfaa91155414ee80",
+    "url": "/clipboard-landing-page/static/media/logo.28caa024.svg"
+  },
+  {
+    "revision": "a030348e47bd6610cc92ab378a8d6059",
+    "url": "/clipboard-landing-page/static/media/image-computer.a030348e.png"
+  },
+  {
+    "revision": "6208ead7a0a569d9746e820ee58f5771",
+    "url": "/clipboard-landing-page/static/media/image-devices.6208ead7.png"
+  },
+  {
+    "revision": "784789634d6cfa0f540b90d2fe91a0b0",
+    "url": "/clipboard-landing-page/static/media/icon-blacklist.78478963.svg"
+  },
+  {
+    "revision": "d9185a429e69ffbac9f83bd9b7586c68",
+    "url": "/clipboard-landing-page/static/media/icon-text.d9185a42.svg"
+  },
+  {
+    "revision": "ee6850bcfba7ae359a07e72ff5a5ad7e",
+    "url": "/clipboard-landing-page/static/media/icon-preview.ee6850bc.svg"
+  },
+  {
+    "revision": "99ef83bcd798a76238f44aee35ed3f7f",
+    "url": "/clipboard-landing-page/static/media/icon-facebook.99ef83bc.svg"
+  },
+  {
+    "revision": "799c9975d592063e2995e272d7344607",
+    "url": "/clipboard-landing-page/static/media/icon-twitter.799c9975.svg"
+  },
+  {
+    "revision": "0f2e9e2537a99944fd45700c0931028c",
+    "url": "/clipboard-landing-page/static/media/icon-instagram.0f2e9e25.svg"
+  },
+  {
+    "revision": "806276512d2662e155c44b2cc5c98bc1",
+    "url": "/clipboard-landing-page/static/media/bg-header-mobile.80627651.png"
+  },
+  {
+    "revision": "df6601b0961197fd570b44815ac44c68",
+    "url": "/clipboard-landing-page/static/media/bg-header-desktop.df6601b0.png"
+  },
+  {
+    "revision": "cbb9c050951c7bdfa8bf33731abd7be9",
+    "url": "/clipboard-landing-page/index.html"
+  }
+];
